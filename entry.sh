801ded2ddff15129ee3aa577fcdc8e328424ad44
Joe Stubbs <joestubbs5@gmail.com>
2023-03-14 16:02:35 -0500

    fail fast when site router API cannot come up.

--- entry.sh
+++ entry.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
-cd /home/tapis; uvicorn service.api:app --reload --host 0.0.0.0 --port 8000
+cd /home/tapis; uvicorn service.api:app --host 0.0.0.0 --port 8000
 
-while true; do sleep 86400; done
+if [ "$DEBUG_SLEEP_LOOP" == "sleep" ]; then
+  while true; do sleep 86400; done
+fi
